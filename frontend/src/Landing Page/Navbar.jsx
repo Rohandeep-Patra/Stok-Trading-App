ee9fb695f471dfe78bd8b48ab304c555f226a7fc
Rohandeep Patra <rohandp100@gmail.com>
2024-08-23 15:31:57 +0530

    add nav and open acc

--- frontend/src/Landing Page/Navbar.jsx
+++ frontend/src/Landing Page/Navbar.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container p-2">
+          <a class="navbar-brand" href="#">
+            <img src="images/logo.svg" style={{ width: "20%" }} />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active text-muted fw-normal" aria-current="page" href="#">
+                    Sign Up
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active text-muted fw-normal" href="#">
+                    About
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active text-muted fw-normal" href="#">
+                    Products
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active text-muted fw-normal" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active text-muted fw-normal" href="#">
+                    Support
+                  </a>
+                </li>
+                
+               
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
   );
-}
+};
 
 export default Navbar;
--- frontend/src/Landing Page/OpenAccount.jsx
+++ frontend/src/Landing Page/OpenAccount.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const OpenAccount = () => {
   return (
-    <div>
-      <h1>Open Acc</h1>
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-2 text-muted">Open a Zerodha account</h1>
+        <p className='my-4 text-muted'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+        <button style={{width: "15%" , margin: "0 auto" , backgroundColor:"#478CCF"}} className="p-2 btn btn-primary mt-2">Sign Up Now</button>
+      </div>
     </div>
   );
 }
